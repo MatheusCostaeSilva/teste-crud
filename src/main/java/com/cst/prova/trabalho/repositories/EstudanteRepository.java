9b5ba4de954ec31b3865493fe26f501565bb9000
unknown <matheuscostaesilva12@gmail.com>
2026-02-26 21:49:20 -0300

    services criado

--- src/main/java/com/cst/prova/trabalho/repositories/EstudanteRepository.java
+++ src/main/java/com/cst/prova/trabalho/repositories/EstudanteRepository.java
@@ -2,7 +2,6 @@ package com.cst.prova.trabalho.repositories;
 
 import com.cst.prova.trabalho.models.EstudanteModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.support.JpaEntityInformation;
 
 public interface EstudanteRepository extends JpaRepository<EstudanteModel, Long> {
 }
---
+++ src/main/java/com/cst/prova/trabalho/services/EstudanteServices.java
@@ -0,0 +1,27 @@
+package com.cst.prova.trabalho.services;
+
+import com.cst.prova.trabalho.models.EstudanteModel;
+import com.cst.prova.trabalho.repositories.EstudanteRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EstudanteServices {
+
+    private static final Logger log = LoggerFactory.getLogger(EstudanteServices.class);
+    @Autowired
+    private EstudanteRepository estudanteRepository;
+
+    public EstudanteModel criarEstudante(EstudanteModel estudanteModel){return EstudanteRepository.save(EstudanteModel);}
+
+    public List<EstudanteModel>  ListarTodosEstudantes(){return EstudanteRepository.findAll();}
+
+    public void deletarAluno(Long id){
+        estudanteRepository.deleteById(id);
+    }
+
+}
